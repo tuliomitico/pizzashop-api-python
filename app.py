5ea07895b7f08095fea407921ed7132d79224ce8
tuliomitico <tulioneedforspeed@gmail.com>
2024-02-15 16:22:44 -0300

    fix: fix hostname error and add docker compose

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def getHello():
     return {"message": "Hello World!"}, 200
 
 if __name__ == "__main__":
-    app.run(host="[::1]",port=3333,debug=True,load_dotenv=True)
+    app.run(host="0.0.0.0",port=3333,debug=True,load_dotenv=True)
